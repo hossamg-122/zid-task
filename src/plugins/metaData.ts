c404ac62db202ad90182a22e3d5e4a733370e697
Hossam Gamal Ali <79874817+hossamg-122@users.noreply.github.com>
2022-08-08 00:19:55 +0200

    feat: pages meta tags

---
+++ src/plugins/metaData.ts
@@ -0,0 +1,15 @@
+export const home = {
+  title: "Home - زد",
+  description:
+    "Zid is a Saudi holding company, established in 2017, aiming to enable the retail sector to enter the world of electronic retail in an easy and professional way.",
+};
+export const login = {
+  title: "Login - زد",
+  description:
+    "Zid is a Saudi holding company, established in 2017, aiming to enable the retail sector to enter the world of electronic retail in an easy and professional way.",
+};
+export const register = {
+  title: "Register - زد",
+  description:
+    "Zid is a Saudi holding company, established in 2017, aiming to enable the retail sector to enter the world of electronic retail in an easy and professional way.",
+};
